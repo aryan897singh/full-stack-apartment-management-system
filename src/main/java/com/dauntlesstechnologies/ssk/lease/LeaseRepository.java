34c56e13766f8426677116b453899e79c1ebe3c0
Aryan Singh <aryan897work@gmail.com>
2026-04-04 22:59:29 -0700

    feat(lease): create custom mysql query to quickly lookup the last occupied date of the apartment | Pro: Extremely fast because we delegate the task to SQL | Con: Code becomes non-db agnostic making migrating database env more troubling in the future

--- src/main/java/com/dauntlesstechnologies/ssk/lease/LeaseRepository.java
+++ src/main/java/com/dauntlesstechnologies/ssk/lease/LeaseRepository.java
@@ -1,12 +1,19 @@
 package com.dauntlesstechnologies.ssk.lease;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.Optional;
 
 
 @Repository
 public interface LeaseRepository extends JpaRepository<Lease, Long> {
     Optional<Lease> findByApartmentIdAndIsActiveTrue(Long apartmentId);
+
+    // Writing custom query to delegate the search to SQL - Pro: Extremely fast Con: Becomes Non DB Agnostic due to custom MySQL query
+    @Query("SELECT MAX(l.end) FROM Lease l WHERE l.apartment.id = :apartmentId AND l.isActive = false")
+    Optional<Date> findLastOccupiedDateByApartmentId(@Param("apartmentId") Long apartmentId);
 }
